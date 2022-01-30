695be50fcbd93aba96bda3802e135303de96a239
sbk0716 <uhey0716@gmail.com>
2022-01-30 12:04:27 +0900

    remove eslintConfig from package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,21 +4,34 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'airbnb',
+    /**
+     * [default extends]
+     * extends moved from eslintConfig in package.json.
+     */
+    "react-app",
+    "react-app/jest",
+    /**
+     * [default extends]
+     * extends added when executing `eslint --init` command.
+     */
+    "plugin:react/recommended",
+    "airbnb",
+    /**
+     * [custom extends]
+     * Set eslint-config-prettier.
+     * add "prettier" to the "extends" array in your .eslintrc.* file.
+     * Make sure to put it last, so it gets the chance to override other configs.
+     */
+    "prettier",
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
-  rules: {
-  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {},
 };
